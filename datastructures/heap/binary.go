fecf14f64b8e738a06669b7aba7e31e8832d33b1
GeorgeErickson <george.erickson.jr@gmail.com>
2016-03-06 19:28:50 -0500

    Heap insert

--- datastructures/heap/binary.go
+++ datastructures/heap/binary.go
@@ -1,1 +1,39 @@
 package heap
+
+// DominateFunc returns true if x dominates y
+type DominateFunc func(x, y int) bool
+
+func Min(x, y int) bool {
+	return x <= y
+}
+
+type Binary struct {
+	dominates DominateFunc
+	data      []int
+}
+
+func NewMinBinary() *Binary {
+	return &Binary{
+		dominates: Min,
+		data:      []int{},
+	}
+}
+
+func (b *Binary) Insert(v int) {
+	b.data = append(b.data, v)
+	vIdx := len(b.data) - 1
+	pIdx := vIdx
+
+	for pIdx > 0 {
+		pIdx = pIdx / 2
+		p := b.data[pIdx]
+
+		if b.dominates(p, v) {
+			return
+		}
+
+		b.data[vIdx], b.data[pIdx] = b.data[pIdx], b.data[vIdx]
+		vIdx = pIdx
+	}
+
+}
--- datastructures/heap/binary_test.go
+++ datastructures/heap/binary_test.go
@@ -1,1 +1,20 @@
 package heap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBinary(t *testing.T) {
+	b := NewMinBinary()
+	assert := require.New(t)
+	b.Insert(7)
+	assert.Equal([]int{7}, b.data)
+	b.Insert(5)
+	assert.Equal([]int{5, 7}, b.data)
+	b.Insert(4)
+	assert.Equal([]int{4, 5, 7}, b.data)
+	b.Insert(6)
+	assert.Equal([]int{4, 5, 7, 6}, b.data)
+}
